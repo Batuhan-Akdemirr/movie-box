aa2db3a3963b8b0c229bd0a384574791915634b3
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-12-25 14:32:26 +0300

    add : SharedManager initiliazed in main.dart

--- movie_box_application/lib/main.dart
+++ movie_box_application/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:movie_box_application/core/app.dart';
+import 'package:movie_box_application/data/local/shared_manager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedManager.initSharedPreferences();
   runApp(const App());
 }
-
