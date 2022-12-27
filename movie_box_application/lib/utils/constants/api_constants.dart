19432ba293f206d04ea0925d3d280507787996bd
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-12-27 13:20:11 +0300

    fix : Added language query parameters

--- movie_box_application/lib/utils/constants/api_constants.dart
+++ movie_box_application/lib/utils/constants/api_constants.dart
@@ -3,11 +3,10 @@ class APIURL {
   static const baseApiUrl = 'https://api.themoviedb.org/3/';
   static const baseImageUrl = 'https://image.tmdb.org/t/p/w300';
   static const queryApiKeyValue = '?api_key=';
-  static const pageUrl = '&page=1';
-  static const searchMovieDetailUrl = '&include_adult=false&query=';
+  static const searchMovieDetailUrl = '&query=';
+  static const language = '&language=';
 }
 
 enum BaseCategoryName { movie, search }
 
 enum MovieCategoryName { upcoming, popular, similar }
-
