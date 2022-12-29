f143ea61685ff8e9cc75cc1703acd2e704e107a1
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-12-29 15:06:57 +0300

    fix : MovieModel  was created without using code generator

--- movie_box_application/lib/core/models/movie_model.dart
+++ movie_box_application/lib/core/models/movie_model.dart
@@ -1,9 +1,6 @@
 
-import 'package:json_annotation/json_annotation.dart';
+// Filmlerin modeli
 
-part  'movie_model.g.dart';
-
-@JsonSerializable()
 class MovieModel {
   bool? adult;
   String? backdropPath;
@@ -36,11 +33,39 @@ class MovieModel {
       this.voteAverage,
       this.voteCount});
 
-   factory MovieModel.fromJson(Map<String, dynamic> json) {
-    return _$MovieModelFromJson(json);
+  MovieModel.fromJson(Map<String, dynamic> json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'].cast<int>();
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
   }
 
   Map<String, dynamic> toJson() {
-    return _$MovieModelToJson(this);
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['adult'] = adult;
+    data['backdrop_path'] = backdropPath;
+    data['genre_ids'] = genreIds;
+    data['id'] = id;
+    data['original_language'] = originalLanguage;
+    data['original_title'] = originalTitle;
+    data['overview'] = overview;
+    data['popularity'] = popularity;
+    data['poster_path'] = posterPath;
+    data['release_date'] = releaseDate;
+    data['title'] = title;
+    data['video'] = video;
+    data['vote_average'] = voteAverage;
+    data['vote_count'] = voteCount;
+    return data;
   }
 }
